1037e934c33a4e05bc0bcfe80b37d69e8d682603
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-02-09 10:15:16 -0300

    Date Picker

---
+++ app/src/main/java/com/hortis/hackathon24/components/datepicker/DatePicker.kt
@@ -0,0 +1,103 @@
+package com.hortis.hackathon24.components.datepicker
+
+import android.graphics.Color.parseColor
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePicker(
+    modifier: Modifier = Modifier
+) {
+    var showDatePickerDialog by remember {
+        mutableStateOf(false)
+    }
+    var selectedDate by remember {
+        mutableStateOf("")
+    }
+    val datePickerState = rememberDatePickerState()
+    if (showDatePickerDialog) {
+        DatePickerDialog(
+            onDismissRequest = { showDatePickerDialog = false },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        datePickerState
+                            .selectedDateMillis?.let { millis ->
+                                selectedDate = millis.toBrazilianDateFormat()
+                            }
+                        showDatePickerDialog = false
+                    }) {
+                    Text(text = "Escolher data")
+                }
+            }) {
+            DatePicker(state = datePickerState)
+        }
+    }
+    TextField(
+        value = selectedDate,
+        onValueChange = { },
+        Modifier
+            .fillMaxWidth()
+            .onFocusEvent {
+                if (it.isFocused) {
+                    showDatePickerDialog = true
+                }
+            }
+            .heightIn(min = 56.dp)
+            .border(
+                width = 1.dp,
+                color = Color(parseColor("#033F63")),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(
+                start = 24.dp,
+                end = 24.dp
+            ),
+        label = {
+            Text("Data")
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.White,
+            disabledContainerColor = Color.White,
+        ),
+        readOnly = true
+    )
+}
+
+fun Long.toBrazilianDateFormat(
+    pattern: String = "dd/MM/yyyy"
+): String {
+    val date = Date(this)
+    val formatter = SimpleDateFormat(
+        pattern, Locale("pt-br")
+    ).apply {
+        timeZone = TimeZone.getTimeZone("GMT")
+    }
+    return formatter.format(date)
+}
